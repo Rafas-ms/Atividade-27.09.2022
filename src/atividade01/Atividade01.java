df12a3a03f7991b0177db3fbc3fffc1fe6d676f8
Rafas-ms <rafael.martinssilva@hotmail.com>
2022-09-27 21:26:37 -0300

    Subindo o codigo

---
+++ src/atividade01/Atividade01.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atividade01;
+
+public class Atividade01 {
+
+    public static void main(String[] args) {
+        Numero n = new Numero();
+        System.out.println("Resultado recursivo: " + n.somaRecursiva(10));
+    }
+    
+}
